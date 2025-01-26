529a59a6e066826931b3becbda28a78f77970c90
Swagatam <swagatamheroz@outlook.com>
2025-01-26 16:00:26 +0530

    feat: add functionality to languages section]

--- src/components/LanguageItem.jsx
+++ src/components/LanguageItem.jsx
@@ -2,11 +2,11 @@ import "../styles/language-item.css";
 
 import { Trash2 } from "lucide-react";
 
-export default function LanguageItem({ language }) {
+export default function LanguageItem({ language, deleteEventHandler }) {
   return (
     <span className="language">
       {language}
-      <Trash2 className="icon" color="#5c677d" />
+      <Trash2 className="icon" color="#5c677d" onClick={deleteEventHandler} />
     </span>
   );
 }
--- src/components/LanguagesEditorSection.jsx
+++ src/components/LanguagesEditorSection.jsx
@@ -1,10 +1,52 @@
 import "../styles/languages-editor-section.css";
 
+import { useState } from "react";
+
 import AddButton from "../ui/AddButton";
 
 import LanguageItem from "./LanguageItem.jsx";
 
+const defaultLanguages = [
+  {
+    id: crypto.randomUUID(),
+    language: "Portuguese",
+  },
+  {
+    id: crypto.randomUUID(),
+    language: "Spanish",
+  },
+  {
+    id: crypto.randomUUID(),
+    language: "Italian",
+  },
+];
+
 export default function LanguagesEditorSection() {
+  const [currentLanguage, setCurrentLanguage] = useState("");
+  const [languages, setLanguages] = useState(defaultLanguages);
+
+  const handleAddLanguage = () => {
+    if (!currentLanguage.trim()) return;
+
+    setLanguages((prevLanguages) => {
+      const newLanguages = structuredClone(prevLanguages);
+      newLanguages.push({ id: crypto.randomUUID(), language: currentLanguage });
+      return newLanguages;
+    });
+
+    setCurrentLanguage("");
+  };
+
+  const handleDeleteLanguage = (targetId) => {
+    setLanguages((prevLanguages) =>
+      prevLanguages.filter((language) => language.id !== targetId)
+    );
+  };
+
+  const handleInput = (event) => {
+    setCurrentLanguage(event.target.value);
+  };
+
   return (
     <section id="languages-editor-section">
       <div>
@@ -14,14 +56,25 @@ export default function LanguagesEditorSection() {
       </div>
 
       <div>
-        <input type="text" placeholder="e.g. Javanese"/>
-        <AddButton label={"Add"} />
+        <input
+          type="text"
+          placeholder="e.g. Javanese"
+          value={currentLanguage}
+          onInput={handleInput}
+        />
+        <AddButton label={"Add"} clickEventHandler={handleAddLanguage} />
       </div>
 
       <div>
-        <LanguageItem language={"Portuguese"} />
-        <LanguageItem language={"Spanish"} />
-        <LanguageItem language={"Italian"} />
+        {languages.map((language) => {
+          return (
+            <LanguageItem
+              key={language.id}
+              language={language.language}
+              deleteEventHandler={() => handleDeleteLanguage(language.id)}
+            />
+          );
+        })}
       </div>
     </section>
   );
